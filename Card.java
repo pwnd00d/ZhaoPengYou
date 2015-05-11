da72c9e76d0e15e9bb44313a7ba97137152cf10d
pwnd00d <austinpwnd00dzhou@gmail.com>
2015-05-11 09:51:30 -0500

    a

---
+++ Card.java
@@ -0,0 +1,32 @@
+
+/**
+ * Write a description of class Card here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Card
+{
+    private int value;
+    private String suit;
+    /**
+     * Ace val = 14
+     * Joker:
+     * suit = null;
+     *small joker, val = 15
+     *big joker, val = 16
+      */
+    public Card(int v,String s)
+    {
+        value = v;
+        suit = s;
+    }
+    public int value()
+    {
+        return value;
+    }
+    public String suit()
+    {
+        return suit;
+    }
+}
--- Deck.java
+++ Deck.java
@@ -2,14 +2,25 @@
 /**
  * Write a description of class Deck here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author (Austin Zhou) 
+ * @version (0)
  */
 public class Deck
 {
+    Card[] deck;
     public Deck()
     {
-        
+        deck = new Card[54];
+        int a = 0;
+        String[] suits = {"hearts","spades","diamonds","clubs"};
+        for(String s:suits)
+        {
+            for(int i = 2;i<15;i++)
+            {
+                deck[a] = new Card(i,s);
+                a++;
+            }
+        }
     }
     public void shuffle()
     {
